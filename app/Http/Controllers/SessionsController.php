11c35ffcfe6fa18da212f91e67c4a0ed258e3839
vagrant <vagrant@homestead>
2020-04-14 07:34:52 +0000

    User log out

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,4 +27,11 @@ class SessionsController extends Controller
             return redirect()->back()->withInput();
         }
     }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', 'You log out successfully');
+        return redirect('login');
+    }
 }
