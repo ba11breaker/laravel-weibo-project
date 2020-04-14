362b7bc4f1895361335a24980cc177e3484867a0
vagrant <vagrant@homestead>
2020-04-14 12:09:09 +0000

    Edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -35,4 +35,29 @@ class UsersController extends Controller
         session()->flash('success', 'Welcome, ' . $request->name);
         return redirect()->route('users.show', [$user]);
     }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6',
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
+
+        session()->flash('success', 'Update Successfully');
+
+        return redirect()->route('users.show', $user->id);
+    }
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -11,7 +11,7 @@
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">Person Center</a>
-              <a class="dropdown-item" href="#">Edit Information</a>
+              <a class="dropdown-item" href="{{ route('users.edit', Auth::user()) }}">Edit Information</a>
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" id="logout" href="#">
               <form action="{{ route('logout') }}" method="POST">
